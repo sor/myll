66c796a491cdd560b72eb7721208f7669c6ebab7
JanSordid <jan@sordid.de>
2020-04-14 19:02:39 +0200

    fix to not crash under netcore 3.1 for linux

--- backend/Grammar/Generated/Myll/MyllParserBaseVisitor.cs
+++ backend/Grammar/Generated/Myll/MyllParserBaseVisitor.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from /home/sor/myll/Antlr/MyllParser.g4 by ANTLR 4.8
+// Generated from /home/sor/myll/backend/Grammar/MyllParser.g4 by ANTLR 4.8
 
 // Unreachable code detected
 #pragma warning disable 0162
